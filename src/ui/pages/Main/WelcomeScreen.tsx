1722377ce694ec09d92377c2b5abb24b27ae22e5
Ben <ben@side.one>
2024-06-18 11:36:01 +0800

    style: welcome page

--- src/ui/pages/Main/WelcomeScreen.tsx
+++ src/ui/pages/Main/WelcomeScreen.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable quotes */
-import { Button, Column, Content, Layout, Row } from '@/ui/components';
+import { Button, Column, Image, Layout, Row } from '@/ui/components';
 import { useExtensionIsInTab } from '@/ui/features/browser/tabs';
 import { useWallet } from '@/ui/utils';
 
@@ -12,38 +12,49 @@ export default function WelcomeScreen() {
 
   return (
     <Layout>
-      <Content preset="middle">
-        <Column fullX>
-          <Row justifyCenter></Row>
-          <Button
-            text="Create new wallet"
-            preset="primary"
-            onClick={async () => {
-              const isBooted = await wallet.isBooted();
-              if (isBooted) {
-                navigate('CreateHDWalletScreen', { isImport: false });
-              } else {
-                navigate('CreatePasswordScreen', { isNewAccount: true });
-              }
-            }}
-          />
-          <Button
-            text="I already have a wallet"
-            preset="default"
-            onClick={async () => {
-              const isBooted = await wallet.isBooted();
-              if (isBooted) {
-                navigate('CreateHDWalletScreen', { isImport: true });
-              } else {
-                navigate('CreatePasswordScreen', { isNewAccount: false });
-              }
-            }}
-            style={{
-              marginTop: '24px'
-            }}
-          />
-        </Column>
-      </Content>
+      <Column
+        fullX
+        fullY
+        style={{
+          padding: '0 16px'
+        }}>
+        <Row
+          justifyCenter
+          style={{
+            flex: 1,
+            alignItems: 'center'
+          }}>
+          <Image src="./images/icons/wallet.svg" size={78} />
+        </Row>
+        <Button
+          text="Create new wallet"
+          preset="primary"
+          onClick={async () => {
+            const isBooted = await wallet.isBooted();
+            if (isBooted) {
+              navigate('CreateHDWalletScreen', { isImport: false });
+            } else {
+              navigate('CreatePasswordScreen', { isNewAccount: true });
+            }
+          }}
+        />
+        <Button
+          text="I already have a wallet"
+          preset="default"
+          onClick={async () => {
+            const isBooted = await wallet.isBooted();
+            if (isBooted) {
+              navigate('CreateHDWalletScreen', { isImport: true });
+            } else {
+              navigate('CreatePasswordScreen', { isNewAccount: false });
+            }
+          }}
+          style={{
+            marginTop: '24px',
+            marginBottom: '24px'
+          }}
+        />
+      </Column>
     </Layout>
   );
 }
