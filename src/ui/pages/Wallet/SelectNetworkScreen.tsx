d7c3257978059d395fa44575f74a56cc8dfdc9da
showzyl <showzyl520@gmail.com>
2024-07-24 22:12:44 +0800

    fix: error

--- src/ui/pages/Wallet/SelectNetworkScreen.tsx
+++ src/ui/pages/Wallet/SelectNetworkScreen.tsx
@@ -1,13 +1,14 @@
 import { useLocation } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
 import { CHAINS_ENUM } from '@/shared/constant';
-import { Column, Content, Header, Image, Layout } from '@/ui/components';
+import { Button, Column, Content, Header, Image, Layout, Row } from '@/ui/components';
 
 import { useNavigate } from '../MainRoute';
 
 export default function SelectNetworkScreen() {
   // TODO: set select network
 
+  const [number, setNumber] = useState(-1);
   const navigate = useNavigate();
   const { state } = useLocation();
   const { type } = state as {
@@ -28,35 +29,56 @@ export default function SelectNetworkScreen() {
         }}>
         <Column
           onClick={() => {
-            navigate('SelectCryptoScreen', {
-              chain: CHAINS_ENUM.BTC,
-              type
-            });
+            // navigate('SelectCryptoScreen', {
+            //   chain: CHAINS_ENUM.BTC,
+            //   type
+            // });
+            setNumber(0);
           }}>
           <Image
             style={{
               cursor: 'pointer'
             }}
             size={'100%'}
-            src="/images/icons/wallet/btc-selected.svg"
+            src={number === 0 ? '/images/icons/wallet/btc-selected.svg' : '/images/icons/wallet/btc-select-dark.svg'}
           />
         </Column>
 
         <Column
           onClick={() => {
-            navigate('SelectCryptoScreen', {
-              chain: CHAINS_ENUM.SIDE,
-              type
-            });
+            // navigate('SelectCryptoScreen', {
+            //   chain: CHAINS_ENUM.SIDE,
+            //   type
+            // });
+            setNumber(1);
           }}>
           <Image
             style={{
               cursor: 'pointer'
             }}
             size={'100%'}
-            src="/images/icons/wallet/side-select-dark.svg"
+            src={number === 1 ? '/images/icons/wallet/side-selected.svg' : '/images/icons/wallet/side-select-dark.svg'}
           />
         </Column>
+        <Row mt={'md'}>
+          <Button
+            full
+            text={'Confirm'}
+            preset="primary"
+            disabled={number === -1}
+            onClick={async () => {
+              if (number === -1) return;
+              let chain = CHAINS_ENUM.BTC;
+              if (number === 1) {
+                chain = CHAINS_ENUM.SIDE;
+              }
+              navigate('SelectCryptoScreen', {
+                chain,
+                type
+              });
+            }}
+          />
+        </Row>
       </Content>
     </Layout>
   );
