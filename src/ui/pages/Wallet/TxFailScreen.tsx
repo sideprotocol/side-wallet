b7cab3a972c8ff8e6b93f94baf0fd819cfc171d7
showzyl <showzyl520@gmail.com>
2024-08-26 17:22:06 +0800

    fix

--- src/ui/pages/Wallet/TxFailScreen.tsx
+++ src/ui/pages/Wallet/TxFailScreen.tsx
@@ -1,14 +1,17 @@
 import { Column, Content, Header, Icon, Layout, Row, Text } from '@/ui/components';
 import { colors } from '@/ui/theme/colors';
 import { useLocationState } from '@/ui/utils';
+import { useNavigate } from '@/ui/pages/MainRoute';
 
 export default function TxFailScreen() {
   const { error } = useLocationState<{ error: string }>();
+  const navigate = useNavigate();
   return (
     <Layout>
       <Header
         onBack={() => {
-          window.history.go(-1);
+          // window.history.go(-1);
+          navigate('MainScreen');
         }}
       />
       <Content>
