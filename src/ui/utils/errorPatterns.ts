b849689dbef822ecc52f525eb4bf2db26a215c96
Max <maxwupro@outlook.com>
2024-09-23 14:00:10 +0800

    format

--- src/ui/utils/errorPatterns.ts
+++ src/ui/utils/errorPatterns.ts
@@ -1,11 +1,11 @@
 export const errorPatterns = {
   sideBTCInsufficientFunds: {
-    pattern: new RegExp("insufficient funds", "i"),
-    message: "Insufficient BTC balance on the Side Chain to cover transaction fees",
+    pattern: new RegExp('insufficient funds', 'i'),
+    message: 'Insufficient BTC balance on the Side Chain to cover transaction fees'
   },
 
   sideBTCVaultNoUTXOs: {
-    pattern: new RegExp("insufficient utxos", "i"),
-    message: "Side Bridge’s BTC vault currently has no usable UTXOs. Please wait and check back later.",
-  },
+    pattern: new RegExp('insufficient utxos', 'i'),
+    message: 'Side Bridge’s BTC vault currently has no usable UTXOs. Please wait and check back later.'
+  }
 };
